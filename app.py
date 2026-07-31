ba905357b35fd2d0b1cebe051a14a132420664e7
singhekas279-maker <singhekas279@gmail.com>
2026-07-31 15:47:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ if all(ALL_API) and user_query:
         response = agent.invoke({'messages':[{'role':"user",
                                               "content":final_prompt}]})
   
-        code = response['messages'] (-1).content[-1]['text']
+        code = response['messages'] [-1].content[-1]['text']
         st.html(code, width="stretch",
                 unsafe_allow_javascript=True)
         
