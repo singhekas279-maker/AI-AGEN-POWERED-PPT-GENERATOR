e4a5f9e1acba8c2cf936a53544dc8d8ec23e61ab
singhekas279-maker <singhekas279@gmail.com>
2026-07-31 16:17:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,9 @@ if all(ALL_API) and user_query:
     if st.button("Click to generate: ", key="generate_img_button"):
       with st.spinner("Running Agent.."):
         data = f"https://image.pollinations.ai/{user_query}"
-        time.sleep(3)
+        import requests as r
+        img_data = r.get(data
+        #time.sleep(3)
         st.image(data)
         
   
